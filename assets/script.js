9284b0ee52a6b59dd4868be28575699b5de43008
HA_GO\antic <nmckinzie@smcm.edu>
2024-11-11 19:18:42 -0600

    began function writing

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,18 @@
-const searchFormEl = document.querySelector('#search-form');
+
+const searchBar = document.getElementbyID('textEntry');
+const form = document.getElementbyID('searchBar');
+const history = document.getElementbyID('giveHistory');
+const currentWeather = document.getElementById('weatherNow');
+const currentForecast = document.getElementById('giveForecast');
+
+
+function grabWeather(city) {
+    const apiKey = 'adc0831491596516de14222646967ab3';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+}
+
+function showWeather
+
+function showForecast
+
+function expandHistory
