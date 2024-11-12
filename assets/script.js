8d50e19df45399e242434aa3befc76dcee5b59ca
HA_GO\antic <nmckinzie@smcm.edu>
2024-11-11 20:08:39 -0600

    javascript programs

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,76 @@
 
-const searchBar = document.getElementbyID('textEntry');
-const form = document.getElementbyID('searchBar');
-const history = document.getElementbyID('giveHistory');
+const searchBar = document.getElementById('textEntry');
+const form = document.getElementById('searchBar');
+const history = document.getElementById('giveHistory');
 const currentWeather = document.getElementById('weatherNow');
 const currentForecast = document.getElementById('giveForecast');
 
+let weatherHistory = [];
+const apiKey = 'adc0831491596516de14222646967ab3';
 
 function grabWeather(city) {
-    const apiKey = 'adc0831491596516de14222646967ab3';
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    const myURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+    fetch(myURL)
+        .then(response => response.json())
+        .then(data => {
+            showWeather(data);
+        })
+};
+
+
+function showWeather(data) {
+    currentWeather.innerHTML = `
+    <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png">
+    <h2>Current weather in ${data.name}</h2>
+    <h3>Temperature: ${data.main.temp}°C</h3>
+    <h4>Humidity: ${data.main.humidity}%</h4>
+    `
+    weatherHistory.push(data.name);
+    showHistory();
 }
 
-function showWeather
 
-function showForecast
+function grabForecast(city) {
+    const forecastLink = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
 
-function expandHistory
+    fetch(forecastLink)
+        .then(response => response.json())
+        .then(data => {
+            showForecast(data);
+        })
+};
+
+function showForecast(data) {
+    currentForecast.innerHTML = `<h5>Five-day forecast:</h5>`;
+    data.list.forEach((item) => {
+        if (item.dt_txt.includes('12:00:00')) {
+            currentForecast.innerHTML += `
+                <div class="forecast-item">
+                    <p><strong>${new Date(item.dt_txt).toLocaleDateString()}</strong></p>
+                    <img src="https://openweathermap.org/img/wn/${item.weather[0].icon}.png" alt="${item.weather[0].description}">
+                    <p>Temperature: ${item.main.temp}°C</p>
+                    <p>Condition: ${item.weather[0].description}</p>
+                </div>
+`;
+        }
+    })
+}
+
+function saveHistory() {
+    history.innerHTML = '<h3>Search History</h3>';
+    weatherHistory.forEach(city => {
+        history.innerHTML += `<p>${city}</p>`;
+    });
+}
+
+const handleSearchFormSubmit = (event) => {
+    event.preventDefault();
+    const city = searchBar.value.trim();
+    if (city) {
+        grabWeather(city); // Fetch weather and forecast for the city
+        searchBar.value = ''
+    }
+}
+
+form.addEventListener('submit', handleSearchFormSubmit);
